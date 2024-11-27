dbe287882b8fd98aa6bf9ee1c3e268be1a58c5a6
Matt <matthew.calabia@founderos.com>
2024-11-28 06:12:39 +0800

    style and js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).on('scroll', function() {
+    if ($(window).scrollTop() > 0) {
+      $('.header').removeClass('transparent').addClass('solid');
+    } else {
+      $('.header').removeClass('solid').addClass('transparent');
+    }
+  });
+  
