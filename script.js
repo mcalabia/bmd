e2d0c2aa37d5b335233da3fe95c5d34f8391283b
Matt <matthew.calabia@founderos.com>
2024-11-29 05:56:38 +0800

    style and js

--- script.js
+++ script.js
@@ -13,13 +13,24 @@ $(window).on('scroll', function() {
     loop:true,
     autoWidth:true,
     nav: false,
-    autoplay:true,
     items:8,
     autoplayTimeout:5000,
     autoplayHoverPause:false,
     slideTransition: 'linear',
     autoplaySpeed: 6000,
-    smartSpeed: 6000
+    smartSpeed: 6000,
+    responsiveClass:true,
+        responsive:{
+            0:{
+              autoplay:true,
+            },
+            767:{
+              autoplay:true,
+            },
+            1200:{
+              autoplay:true,
+            }
+        }
 })
   });
   
