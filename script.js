f8e374328609278820f2f9d971b247011d6e50e6
Matt <matthew.calabia@founderos.com>
2024-11-30 04:59:29 +0800

    animation

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ $(window).on('scroll', function() {
   });
 
   $(document).ready(function () {
-   
   $('.logos-block').owlCarousel({
-    
     nav: false,
     items:6,
     autoplayTimeout:5000,
@@ -36,5 +34,73 @@ $(window).on('scroll', function() {
             }
         }
 })
+
+// ANIMATION
+gsap.registerPlugin(ScrollTrigger);
+
+function initAnimations() {
+  // Navbar animation
+  const navbar = document.querySelector(".header");
+  if(navbar){
+  gsap.fromTo(navbar, 
+      { y: -100 }, 
+      { duration: 1, y: 0, ease: "power2.out" }
+  );
+  }
+
+  // Hero container animation
+  const hero_container = document.querySelector(".title-1");
+  if(hero_container){
+  gsap.fromTo(hero_container, 
+      { opacity: 0 }, 
+      { opacity: 1, duration: 1, ease: "power2.out" }
+  );
+}
+
+  // Slide up the hero description block
+  const hero_description_block = document.querySelector(".hero-description");
+  if(hero_description_block){
+  gsap.fromTo(hero_description_block, 
+      { y: 100, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 1, delay: 0.3, ease: "power2.out" }
+  );
+}
+
+  // Logo carousel container animation
+  const logo_carousel_container = document.querySelector(".logos-container");
+  if(logo_carousel_container){
+  gsap.fromTo(logo_carousel_container, 
+      { opacity: 0 }, 
+      { opacity: 1, duration: 1, delay: 0.8, ease: "power2.out" }
+  );
+}
+}
+
+function animateElementWithScrollTrigger(element, section, delay) {
+  const elementdata = document.querySelector(element);
+if(elementdata){
+  gsap.fromTo(element, 
+      { y: 100, opacity: 0 }, 
+      {
+          y: 0, 
+          opacity: 1, 
+          duration: 1, 
+          delay: delay,
+          ease: "power2.out",
+          scrollTrigger: {
+              trigger: section,   
+              // markers:true,
+              start: "top bottom", 
+              end: "bottom top",
+              toggleActions: "play none none reverse"       
+          }
+      }
+  );
+}
+}
+
+initAnimations();
+animateElementWithScrollTrigger(".text-3-up-content", ".text-3-up-content", .3);
+
   });
   
